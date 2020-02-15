c4fc40ebcf071d173c6ebe26a6fefdd9228930ad
Joshua Liu <liushuya7@gmail.com>
2020-02-15 15:57:58 -0500

    URDF now resembles the real robot

--- ros/src/laser_description/scripts/kinematics_verify_tf.py
+++ ros/src/laser_description/scripts/kinematics_verify_tf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python  
 import roslib
-roslib.load_manifest('verify_tf')
+roslib.load_manifest('laser_description')
 import rospy
 
 import numpy as np
@@ -10,21 +10,21 @@ import tf
 from sensor_msgs.msg import JointState
 
 def broadcast_fwd_kin_pose(msg):
-    Lx = 0.130
-    Ly = -0.165
-    Lz = 0.211
-    l = 0.120
+    Lx = 0.1015
+    Ly = -0.2105
+    Lz = 0.23345
+    l = 0.13145
 
     for i in range(len(msg.name)):
-        if msg.name[i] == "rotary_base__rotary_middle":
+        if msg.name[i] == "A":
             theta4 = -msg.position[i]
-        elif msg.name[i] == "rotary_middle__rotary_top":
+        elif msg.name[i] == "B":
             theta5 = msg.position[i]
-        elif msg.name[i] == "x_box__tool":
+        elif msg.name[i] == "X":
             d1 = abs(msg.position[i])
-        elif msg.name[i] == "y_box1__x_box":
+        elif msg.name[i] == "Y":
             d2 = abs(msg.position[i])
-        elif msg.name[i] == "base_link__y_box1":
+        elif msg.name[i] == "Z":
             d3 = abs(msg.position[i])
 
     tf_L_T = np.array( [ [1,0,0,-d1], [0,1,0,d2], [0,0, 1,-d3], [0,0,0,1]] ).astype(np.float)
@@ -48,15 +48,10 @@ def broadcast_fwd_kin_pose(msg):
     tf_W_T = np.matmul(np.linalg.inv(tf_O_W), tf_O_T)
 
     br = tf.TransformBroadcaster()
-    # br.sendTransform((tf_W_T[0,3], tf_W_T[1,3], tf_W_T[2,3]),
-    #                  tf.transformations.quaternion_from_matrix(tf_W_T),
-    #                  rospy.Time.now(),
-    #                  "tool_tip",
-    #                  "rotary_top")
     br.sendTransform((tf_W_T[0,3], tf_W_T[1,3], tf_W_T[2,3]),
                      tf.transformations.quaternion_from_matrix(tf_W_T),
                      rospy.Time.now(),
-                     "tool_tip",
+                     "tool_kins",
                      "rotary_top")
 
 if __name__ == '__main__':
